d4b4cb6b81d35b4b5a2c024347be1456a3ceb59a
Max Bye <mbye@techsoup.org>
2021-03-26 10:32:47 +0000

    working on warnings

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
-        $("#gmap_canvas").attr("height", window.innerHeight - 350);
-        $("#animationsHere").removeClass();
-        $(".hippoDisclaimer").hide();
-  setTimeout(function () {
-    $("#animationsHere").show().addClass("animate__animated animate__jackInTheBox");
-  }, 2000);
 
+  $( document ).ready(function() {
+    $("#gmap_canvas").attr("height", window.innerHeight - 350);
+    $("#animationsHere").removeClass(); 
+    $(".hippoDisclaimer").hide();
+    setTimeout(function () {
+      $("#animationsHere").show().addClass("animate__animated animate__jackInTheBox");
+    }, 0);
+  });
 
-  function hugThisHippo() {
+  function hideAll(){
+    $(".hippoDetails, .homepage,.hippoDisclaimer").hide();
+    $("#animationsHere").removeClass();
+  }
+
+  function disclaimer() {
     hideAll();
+    $(".hippoContainer, .hippoDisclaimer").show();
     setTimeout(function () {
       $(".hippoDisclaimer, .hippoContainer").show();
     $("#animationsHere").show().addClass("animate__animated animate__jackInTheBox");
@@ -15,16 +23,11 @@
     $(".hippoBg").css({"background": 'url("http://mrmoonhead.com/hugahippo/img/disclaimer.png")', "background-size": 'cover'});
   }
 
-  function hideAll(){
-    $(".homepage, .hippoContainer").hide();
-    $("#animationsHere").removeClass();
-  }
-
   function londonZooDescription() {
+    hideAll();
+    $(".hippoContainer, .hippoDetails").show();
     $(".hippoBg").css({"background": 'url("http://mrmoonhead.com/hugahippo/img/details.png")', "background-size": 'cover'});
-    
-    setTimeout(function () {
-      $(".hippoDisclaimer, .hippoContainer").show();
-    $("#animationsHere").show().addClass("animate__animated animate__jackInTheBox");
-      }, 1000);
-  }
+  } 
+
+  hideAll();
+  londonZooDescription();
