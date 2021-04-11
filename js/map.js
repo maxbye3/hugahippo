61f2db968d34b49161ce1d15c1d443363bc507ab
Max Bye <mbye@techsoup.org>
2021-04-11 14:29:10 +0100

    maps

--- js/map.js
+++ js/map.js
@@ -1,36 +1,63 @@
 function initMap() {
-  const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-    center: { lat: 41.85, lng: -87.65 },
+    zoom: 14,
   });
   directionsRenderer.setMap(map);
+  displayDrivingRoute(directionsService, directionsRenderer);
+  // calculateWalkingRoute(directionsService);
 
-  const onChangeHandler = function () {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  };
-  document.getElementById("start").addEventListener("change", onChangeHandler);
-  document.getElementById("end").addEventListener("change", onChangeHandler);
+  /*   document.getElementById("mode").addEventListener("change", () => {
+      displayDrivingRoute(directionsService, directionsRenderer);
+    }); */
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  directionsService.route(
-    {
-      origin: {
-        query: document.getElementById("start").value,
-      },
-      destination: {
-        query: document.getElementById("end").value,
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
+function calculateWalkingRoute(directionsService) {
+  directionsService.route({
+      origin: "26 St John St, Barbican, London EC1M 4AY",
+      destination: "London Zoo",
+      // Note that Javascript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      travelMode: "WALKING",
     },
     (response, status) => {
-      if (status === "OK") {
-        directionsRenderer.setDirections(response);
-      } else {
-        window.alert("Directions request failed due to " + status);
+      if (status == "OK") {
+        console.log("walking duration: ", response.routes[0].legs[0].duration.text);
       }
-    }
-  );
+    });
+}
+
+
+function displayDrivingRoute(directionsService, directionsRenderer) {
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+
+        directionsService.route({
+            origin: {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                      },
+            destination: "London Zoo",
+            // Note that Javascript allows us to access the constant
+            // using square brackets and a string value as its
+            // "property."
+            travelMode: "DRIVING",
+          },
+          (response, status) => {
+            if (status == "OK") {
+              directionsRenderer.setDirections(response);
+              console.log("driving duration: ", response.routes[0].legs[0].duration.text);
+              /* console.log("walking: 1 hour 6 minutes") */
+              ;
+            } else {
+              window.alert("Directions request failed due to " + status);
+            }
+          }
+        );
+      });
+  }
 }
