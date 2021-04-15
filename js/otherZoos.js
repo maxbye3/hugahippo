aa7b02355368e0101f227f45b3fcd9beaaa6cd35
Max Bye <mbye@techsoup.org>
2021-04-15 21:12:13 +0100

    2 more hippos left

--- js/otherZoos.js
+++ js/otherZoos.js
@@ -1,9 +1,15 @@
 
   $( document ).ready(function() {
+    $("#gmap_canvas").css("height", window.innerHeight - 350);
     hideAll();    
     $(".selectZoo").show(); 
     $(".hippoContainer").show();   
   });
+
+  function selectZoo(id) {
+    console.log(1);
+    $('#gmap_canvas').attr("src",id);
+  }
 
   function hideAll(){
     // $(".selectMap, .selectZoo").hide();
