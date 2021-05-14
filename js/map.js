a1d2023cfe4b744a22dda792669d00b60fc1e53d
Max Bye <mbye@techsoup.org>
2021-05-14 19:39:10 +0100

    tested with niall

--- js/map.js
+++ js/map.js
@@ -3,44 +3,18 @@ function initMap() {
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-   center: { lat: 51.5074, lng: 0.1278 },
+    center: { lat: 51.5074, lng: 0.1278 },
+    zoom: 4,
   });
   directionsRenderer.setMap(map);
-  displayDrivingRoute(directionsService, directionsRenderer);
-  // calculateWalkingRoute(directionsService);
-
-  /*   document.getElementById("mode").addEventListener("change", () => {
-      displayDrivingRoute(directionsService, directionsRenderer);
-    }); */
+  setTimeout(function(){ directionsRenderer.setMap(map); }, 1000);
+  setTimeout(function(){ displayDrivingRoute(directionsService, directionsRenderer); }, 2000);
+  
 }
-
-function calculateWalkingRoute(directionsService) { 
-  const whereAmI =  $("input").val() || "26 St John St, Barbican, London EC1M 4AY";
-  console.log(whereAmI);
-  directionsService.route({
-      origin: whereAmI,
-      destination: "London Zoo",
-      // Note that Javascript allows us to access the constant
-      // using square brackets and a string value as its
-      // "property."
-      travelMode: "WALKING",
-    },
-    (response, status) => {
-      if (status == "OK") {
-        console.log("walking duration: ", response.routes[0].legs[0].duration.text);
-      }
-    });
-}
-
 
 function displayDrivingRoute(directionsService, directionsRenderer) {
     const whereAmI =  $("input").val() || "26 St John St, Barbican, London EC1M 4AY";
-  console.log(whereAmI);
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(
-  //     (position) => {
-  //       console.log(position);
+  // console.log(whereAmI);
         directionsService.route({
             origin: whereAmI,
             destination: "London Zoo",
@@ -62,6 +36,4 @@ function displayDrivingRoute(directionsService, directionsRenderer) {
             }
           }
         );
-      // });
-  // }
 }
